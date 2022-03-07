9339e247cebf53f7d11ac3f144d03e3fe5fcb1b5
NigelKaran <nigel@mctsl.co.uk>
2022-03-07 10:58:05 +0100

    Submission v1

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -9,8 +9,8 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
-  },
+    },
+  
 
   rinkeby: {
     provider: () => new HDWallet({
@@ -19,11 +19,11 @@ module.exports = {
       },
       providerOrUrl: `https://rinkeby.infura.io/v3/${infuraKey}`}),
     network_id: 4,
-    gas: 4500000,
-    gasPrice: 10000000000
+    //gas: 4500000,
+    //gasPrice: 10000000000
     //from: "0x23836FD7b36dDcB410a11D599f6dEe33765B5583"
+    }
   },
-
   // compilers: {
   //   solc: {
   //     version: "^0.4.24",
